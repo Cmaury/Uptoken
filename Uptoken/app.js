b10d46b5876113b491b55ac4bdd630f1e6c29259
cmaury <chrmaury@gmail.com>
2012-05-27 14:39:06 -0700

    add app

---
+++ Uptoken/app.js
@@ -0,0 +1,174 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var UsersDb = require ('./model').UsersDb;
+
+var conf = require('./conf');
+
+var everyauth = require('everyauth')
+  , Promise = everyauth.Promise;
+
+everyauth.debug = true;
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , ObjectId = mongoose.SchemaTypes.ObjectId;
+
+var UserSchema = new Schema({})
+  , User;
+
+var mongooseAuth = require('./mongoose_auth_index');
+
+UserSchema.plugin(mongooseAuth, {
+    everymodule: {
+      everyauth: {
+          User: function () {
+            return User;
+          }
+      }
+    }
+  , facebook: {
+      everyauth: {
+          myHostname: 'http://uptoken.org'
+        , appId: conf.fb.appId
+        , appSecret: conf.fb.appSecret
+        , redirectPath: '/'
+
+      }
+    }
+
+  , password: {
+        loginWith: 'login'
+      , extraParams: {
+          votes: Array,
+      //      phone: String,
+          name: {
+                first: String
+              , last: String
+            }
+        }
+      , everyauth: {
+            getLoginPath: '/login'
+          , postLoginPath: '/login'
+          , loginView: 'login.jade'
+          , getRegisterPath: '/register'
+          , postRegisterPath: '/register'
+          , registerView: 'register.jade'
+          , loginSuccessRedirect: '/'
+          , registerSuccessRedirect: '/'
+        }
+    }
+});
+// Adds login: String
+
+mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost/uptoken');
+
+User = mongoose.model('User');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+//  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+//next
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: conf.mongoose.secret}));
+  app.use(mongooseAuth.middleware());
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+
+var usersDb = new UsersDb('localhost', 27017);
+var userScore = new UserScore('localhost', 27017);
+var tokensDB = new TokensDB('localhost', 27017);
+
+app.get('/', function(req, res){
+        res.render('index.jade', { locals: {
+            title: 'UpToken.org',
+            alert: null
+            }
+        });
+});
+
+app.post('/collect', function(req, res){
+    token = req.body.collect_code;
+    if (req.user== undefined) {
+      res.redirect('/error/1');
+    } 
+    else {
+      user_id = req.user._id.toString()
+      console.log(token);
+      tokensDB.isValid(user_id, token, function(alert) {
+        console.log("redirect " + alert);
+        if (alert == null) {
+          userScore.increment(user_id, function(response){
+              res.redirect('/user/Karma-Collected');
+          });
+        }
+        else  {
+          res.redirect('/error/' + alert)
+        }
+      });
+    };
+});
+
+app.get('/user/:alert', function(req, res){
+    user_id = req.user._id.toString()
+    console.log("routes " + user_id);
+    userScore.findOne(user_id, function(error, results) {
+      res.render("user.jade", {locals: {
+        title: req.user.login + "'s Profile",
+        score: results.score,
+        alert: req.params.alert
+      }
+    });
+    })
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/error/1', function (req, res) {
+    res.render("register.jade", {locals: {
+        title: "Oops",
+        error: "sign up to collect your upvote."
+      }
+    });
+});
+
+app.get('/error/:alert', function (req, res) {
+    res.render("index.jade", {locals: {
+        title: "Oops",
+        alert: req.params.alert
+      }
+    });
+});
+
+
+mongooseAuth.helpExpress(app);
+
+app.listen(4000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ Uptoken/model.js
@@ -0,0 +1,145 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+
+//TokensDB
+TokensDB = function(host, port) {
+  this.db= new Db('uptoken', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+TokensDB.prototype.getCollection= function(callback) {
+  this.db.collection('token', function(error, token_collection) {
+    if( error ) callback(error);
+    else callback(null, token_collection);
+  });
+};
+
+TokensDB.prototype.isValid = function(user_id, token, callback) {
+    this.getCollection(function(error, token_collection) {
+      if( error ) callback(error)
+      else {
+        token_collection.findOne({'token': token}, function(error, token_data) {
+          if( token_data == null ) callback('Invalid Token')
+          else TokensDB.prototype.lastUser(token_collection, user_id, token_data, callback)
+        });
+      }
+    });
+};
+
+TokensDB.prototype.lastUser = function(token_collection, user_id, token_data, callback) {
+        token_collection.findOne({'token':token}, function(error, token) {
+          console.log("1 " + user_id)
+          if( user_id == token_data.users[token_data.users.length -1] ) callback('Bad Karma. You can\'t upvote yourself!')
+          else TokensDB.prototype.upVote(token_collection, user_id, token_data, callback)
+        });
+      };
+
+TokensDB.prototype.upVote = function(token_collection, user_id, token_data, callback) {
+  token_collection.update({ "token": token_data.token}, {$push: {"users": user_id, "timestamp":new Date()}}, function(error, token) {
+          console.log("2 " + user_id)
+          callback(null);
+        });
+      };
+
+
+//User Score
+UserScore = function(host, port) {
+  this.db= new Db('uptoken', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+UserScore.prototype.getCollection= function(callback) {
+  this.db.collection('userScore', function(error, userScore_collection) {
+    if( error ) callback(error);
+    else callback(null, userScore_collection);
+  });
+};
+
+UserScore.prototype.findOne = function(user_id, callback) {
+    this.getCollection(function(error, userScore_collection) {
+      if( error ) callback(error)
+      else {
+        console.log("user "+ user_id);
+        userScore_collection.findOne({"user_id": user_id}, function(error, results) {
+          console.log("score " + results.score);
+        if( error ) callback(error);
+        else callback(null, results);
+        });
+      }
+    });
+};
+
+UserScore.prototype.increment = function(user_id, callback) {
+    this.getCollection(function(error, userScore_collection) {
+      if( error ) callback(error)
+      else {
+        console.log("3 " + user_id)
+        userScore_collection.update({"user_id": user_id}, {$inc: {"score": 1}}, true, function(error, results) {
+          console.log("3 "+ results );
+          if( error ) callback(error)
+          else  callback(null)
+        });
+      }
+    });
+};
+
+//Users
+UsersDb = function(host, port) {
+  this.db= new Db('uptoken', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+UsersDb.prototype.getCollection= function(callback) {
+  this.db.collection('users', function(error, user_collection) {
+    if( error ) callback(error);
+    else callback(null, user_collection);
+  });
+};
+
+UsersDb.prototype.findAll = function(callback) {
+    this.getCollection(function(error, user_collection) {
+      if( error ) callback(error)
+      else {
+        user_collection.find().toArray(function(error, results) {
+          console.log(results);
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+UsersDb.prototype.collect = function(userId, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user_collection.update({"_id": user_collection.db.bson_serializer.ObjectID.createFromHexString(userId)}, "$score", upsert)
+      console.log(user_collection.findOne({"_id": user_collection.db.bson_serializer.ObjectID.createFromHexString(userId)}))
+      if( error ) callback(error)
+      else callback(null, results)
+    }  
+  });
+};
+
+
+UsersDb.prototype.findUser = function(userId, callback) {
+    this.getCollection(function(error, user_collection) {
+      console.log(userId)
+      if( error ) callback(error)
+      else {
+        user_collection.findOne({"_id": user_collection.db.bson_serializer.ObjectID.createFromHexString(userId)}, function(error, results) {
+          console.log("hello" + results);
+          if( error ) callback(error)
+          else callback(null, results, ideas)
+        });
+      }
+    });
+};
+
+exports.UsersDb = UsersDb;
+exports.UserScore = UserScore;
+exports.TokensDB = TokensDB;
