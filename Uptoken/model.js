99448b2387a75e799fcc2cd3c11e43deab8c5916
cmaury <chrmaury@gmail.com>
2012-06-30 14:06:53 -0700

    test: user still not saving

--- Uptoken/model.js
+++ Uptoken/model.js
@@ -66,7 +66,7 @@ UserScore.prototype.findOne = function(user_id, callback) {
         console.log("user "+ user_id);
         userScore_collection.findOne({"user_id": user_id}, function(error, results) {
         if(!results) {
-           userScore_collection.update({"user_id": user_id}, {"score": 0}, true,  function(error, results) {
+           userScore_collection.update({"user_id": user_id}, {$inc: {"score": 1}}, true,  function(error, results) {
            //console.log("result " + results)
            if( error) callback(error);
            else callback(null, results);
