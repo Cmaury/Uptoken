8c053a533484bc451e535636c9105e5acbac000a
cmaury <chrmaury@gmail.com>
2012-06-30 17:29:56 -0700

    bandaid

--- Uptoken/app.js
+++ Uptoken/app.js
@@ -125,7 +125,7 @@ app.post('/collect', function(req, res){
         if (alert == null) {
           userScore.increment(user_id, function(error, results){
               res.render('user.jade', {locals: {
-                score: results.score,
+                score: "results.score",
                 alert: 'Karma-Collected'
               }});
           });
