ad3859e6055dbf72b5484e2606f41f064f300c01
cmaury <chrmaury@gmail.com>
2012-06-30 18:54:20 -0700

    i hate this shit

--- Uptoken/app.js
+++ Uptoken/app.js
@@ -142,7 +142,7 @@ app.get('/user/profile', function(req, res){
     user_id = req.user._id.toString()
     //console.log("routes " + user_id);
     userScore.findOne(user_id, function(error, results) {
-      if(!results.score){
+      if(!results){
         console.log("no results")
           score= 0
         }
